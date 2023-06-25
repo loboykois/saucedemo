7b0ee77f292b27b6984b6673280a7a1a4992a001
loboykois <loboykois@gmail.com>
2023-06-25 15:05:02 +0300

    test(SLLPTS-010): add negative test case to Swag Labs test suit

--- pageObjects/loginForm.js
+++ pageObjects/loginForm.js
@@ -1,5 +1,7 @@
 export class LoginForm {
+  #page;
   constructor(page) {
+    this.#page = page;
     this.userNameField = page.locator("[data-test='username']");
     this.passwordField = page.locator("[data-test='password']");
     this.loginButton = page.locator("[data-test=login-button]");
@@ -7,5 +9,11 @@ export class LoginForm {
 
   async pressLoginButton() {
     await this.loginButton.click();
+  }
+
+  async triggeredErrorText() {
+    const errorText = await this.#page.locator("[data-test='error']");
+   //  console.info("ERROR TEXT" + errorText);
+    return errorText;
   }
 }
--- tests/loginPageTests.spec.js
+++ tests/loginPageTests.spec.js
@@ -16,4 +16,33 @@ test.describe("Login page Swag Labs Functional tests", () => {
     await loginPage.loginForm.pressLoginButton();
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
   });
+
+  test("when entered valid username and invalid password error message should be showed", async ({
+    page,
+  }) => {
+    const loginPage = new SwagLabsLoginPage(page);
+    await loginPage.visitLoginPage();
+    const userName = await loginPage.pageLegend.getUserNameByType("standard");
+    await loginPage.enterUserName(userName);
+    const password = await loginPage.pageLegend.getPassword();
+    await loginPage.enterPassword(password + "123");
+    await loginPage.loginForm.pressLoginButton();
+    const errorText = await loginPage.loginForm.triggeredErrorText();
+    await expect(errorText).toBeVisible();
+  });
+
+  test("when valid username and invalid password are entered error message should be match to approved text", async ({
+    page,
+  }) => {
+    const loginPage = new SwagLabsLoginPage(page);
+    await loginPage.visitLoginPage();
+    const userName = await loginPage.pageLegend.getUserNameByType("standard");
+    await loginPage.enterUserName(userName);
+    const password = await loginPage.pageLegend.getPassword();
+    await loginPage.enterPassword(password + "123");
+    await loginPage.loginForm.pressLoginButton();
+    const approvedErrorMessageText =
+      "Epic sadface: Username and password do not match any user in this service";
+    // finish test case
+  });
 });
